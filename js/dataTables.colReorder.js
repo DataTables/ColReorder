607bae66f2febc0ddda67ff9fcb7efa564f56246
Allan Jardine <allan@stargazer>
2017-08-30 15:21:57 +0100

    ColReorder 1.4.0 release

--- js/dataTables.colReorder.js
+++ js/dataTables.colReorder.js
@@ -1,15 +1,15 @@
-/*! ColReorder 1.3.3
- * ©2010-2015 SpryMedia Ltd - datatables.net/license
+/*! ColReorder 1.4.0
+ * ©2010-2017 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     ColReorder
  * @description Provide the ability to reorder columns in a DataTable
- * @version     1.3.3
+ * @version     1.4.0
  * @file        dataTables.colReorder.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
- * @copyright   Copyright 2010-2014 SpryMedia Ltd.
+ * @copyright   Copyright 2010-2017 SpryMedia Ltd.
  *
  * This source file is free software, available under the following license:
  *   MIT license - http://datatables.net/license/mit
@@ -1259,7 +1259,7 @@ ColReorder.defaults = {
  *  @type      String
  *  @default   As code
  */
-ColReorder.version = "1.3.3";
+ColReorder.version = "1.4.0";
 
 
 
