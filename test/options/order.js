a9cc02b077990bec7c8e9abdcd05b8bd6a81235d
Colin Marks <colin@datatables.net>
2018-12-06 15:36:00 +0000

    DEV some more unit tests and doc mods

---
+++ test/options/order.js
@@ -0,0 +1,27 @@
+describe('colReorder - order', function() {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'colreorder'],
+		css: ['datatables', 'colreorder']
+	});
+
+	dt.html('basic');
+	it('No order set by default', function() {
+		table = $('#example').DataTable({
+			colReorder: true
+		});
+		expect($.fn.dataTable.ColReorder.defaults.aiOrder).toBe(null);
+		expect($('thead tr th').text()).toBe('NamePositionOfficeAgeStart dateSalary');
+	});
+
+	dt.html('basic');
+	it('Order complied with', function() {
+		table = $('#example').DataTable({
+			colReorder: {
+				order: [5,4,3,2,1,0]
+			}
+		});
+		expect($('thead tr th').text()).toBe('SalaryStart dateAgeOfficePositionName');
+	});
+});
