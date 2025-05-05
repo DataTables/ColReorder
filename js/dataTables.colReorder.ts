885ecdc5fbac504e82b8b032e029124717f1f416
Allan Jardine <allan@datatables.net>
2025-05-05 09:16:58 +0000

    Release 2.1.0

--- js/dataTables.colReorder.ts
+++ js/dataTables.colReorder.ts
@@ -1,11 +1,11 @@
-/*! ColReorder 2.1.0-dev
+/*! ColReorder 2.1.0
  * © SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     ColReorder
  * @description Provide the ability to reorder columns in a DataTable
- * @version     2.1.0-dev
+ * @version     2.1.0
  * @author      SpryMedia Ltd
  * @contact     datatables.net
  * @copyright   SpryMedia Ltd.
