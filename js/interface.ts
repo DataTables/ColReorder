b2af8c3d7576231830b723314a74952baecd87fa
Allan Jardine <allan@datatables.net>
2026-09-25 08:53:23 +0000

    Fix: Typescript - state save object doesn't have the colReorder property

--- js/interface.ts
+++ js/interface.ts
@@ -44,6 +44,20 @@ declare module 'datatables.net' {
 	interface ColumnContext {
 		_crOriginalIdx?: number;
 	}
+
+	interface State {
+		/**
+		 * Column ordering position
+		 */
+		colReorder?: number[];
+	}
+
+	interface StateLoad {
+		/**
+		 * Column ordering position
+		 */
+		colReorder?: number[];
+	}
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
